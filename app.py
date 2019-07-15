db8a51aab88f86ca5b3edf2fd36e5c595b2f6e28
anshnagrath <anshnagrath448@gmail.com>
2019-07-15 11:32:34 +0530

    started

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from user import User
+from database import Database
+db = Database()
+db.initialise(user='postgres', password='wezzy33beat',database='learning',host='localhost')
+user = User('ansh@gmail.com','ansh','nagrath',None)
+user.save_to_db()
 my_user = User.load_from_db_email('anshnagrath@gmail.com')
 print(my_user)
--- database.py
+++ database.py
@@ -1,13 +1,34 @@
 from psycopg2 import pool
+
 # using one pool connection througout
-cp =   pool.SimpleConnectionPool(1,1,user='postgres', password='wezzy33beat',database='learning',host='localhost')
+class Database:
+    cp = None
+    def initialise(self, **kwargs):
+        cp =  pool.SimpleConnectionPool(1,1,**kwargs)
+
+    @classmethod
+    def get_connection(cls):
+        return cls.cp.getconn()
+
+    @classmethod
+    def returnconnection(cls,connection):
+        Database.cp.putconn(connection)  
+
+    @classmethod
+    def closeallconnection(cls):
+        cls.cp.closeall()      
+
 class ConnectionFromPool:
+    global cp  
+    cp =  pool.SimpleConnectionPool(1,1,user='postgres', password='wezzy33beat',database='learning',host='localhost')
     def __init__(self):
        self.connection = None
-
     def __enter__(self):
         self.connection = cp.getconn()    
         return self.connection
     def __exit__(self,exc_type,exc_val,exc_tb):
-        self.connection.commit()
-        cp.putconn(self.connection)
+        if exc_val is not None:
+            self.connection.rollback()
+        else:    
+            self.connection.commit()
+            cp.putconn(self.connection)
